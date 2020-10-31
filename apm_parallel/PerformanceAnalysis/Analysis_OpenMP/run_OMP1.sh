8354289090ded67cde08b053af3c2763d9475650
Fatma BOUZGHAIA <fatma.bouzghaia@telecom-sudparis.eu>
2020-10-31 19:36:58 +0100

    Fixing MakeFile

--- apm_parallel/PerformanceAnalysis/Analysis_OpenMP/run_OMP1.sh
+++ apm_parallel/PerformanceAnalysis/Analysis_OpenMP/run_OMP1.sh
@@ -3,12 +3,15 @@
 export GOMP_CPU_AFFINITY=`seq -s"," 0 47`
 DISTANCE=0
 DNA_FILE="../dna/small_chrY.fa"
-DNA_PATTERN="GCC ATT AAA GGG CCC TAT ATC GTC"
+DNA_PATTERN="GCC ATT AAA GGG CCC TAT ATC GTC TTT CGT"
 t_seq=""
-cd ../../
+
+cd ..
+cd ..
 
 for i in $(seq 1 48) ; do
-    t_parallel=$(./apmOpenMP2 $i $DISTANCE $DNA_FILE $DNA_PATTERN | grep APM | awk '{print $4}')
+    export OMP_NUM_THREADS=$i
+    t_parallel=$(./apm_OpenMP_pattern_sharing $DISTANCE $DNA_FILE $DNA_PATTERN  | grep APM | awk '{print $4}')
     if [ $i -eq  1 ]; then
         t_seq=$t_parallel
     fi
